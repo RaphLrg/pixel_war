a773d0695df42447de491dd61bea6aac75654755
RaphLrg <144760832+RaphLrg@users.noreply.github.com>
2023-10-10 20:33:17 -0400

    Add files via upload

---
+++ backend/db/remplir_database.sql
@@ -0,0 +1,29 @@
+INSERT INTO user 
+(
+	pseudo,
+	pwdHash,
+	vipLevel,
+	avatarId,
+	colorStats,
+	nbModif,
+	lastModifTime
+) 
+VALUES 
+(
+	"Revilero",
+	"1OM3NO9EN1",
+	1,
+	0,
+	"3:12,5:8,6:5,8:24",
+	56,
+	10
+);
+
+INSERT INTO colors (colorCode) VALUES ("#FFFFFF");
+INSERT INTO colors (colorCode) VALUES ("#000000");
+INSERT INTO colors (colorCode) VALUES ("#FF0000");
+INSERT INTO colors (colorCode) VALUES ("#00FF00");
+INSERT INTO colors (colorCode) VALUES ("#0000FF");
+INSERT INTO colors (colorCode) VALUES ("#00FFFF");
+INSERT INTO colors (colorCode) VALUES ("#FF00FF");
+INSERT INTO colors (colorCode) VALUES ("#FFFF00");
